b140a0bca97b6b13f13fdfd3ad5eba4bd264adde
tobiasbrammer <70036361+tobiasbrammer@users.noreply.github.com>
2023-01-03 14:40:41 +0100

    Update

--- Problem Sets/Problem Set 7/Problem Set 7.R
+++ Problem Sets/Problem Set 7/Problem Set 7.R
@@ -35,7 +35,7 @@ copT <- Estimate_Patton(CopType = "t", mY = dj_returns)
 if(!require(ggplot2)){install.packages('ggplot2')}
 if(!require(ggthemes)){install.packages('ggthemes')}
 df <- data.frame(
-    time = as.Date(rownames(dj_returns)),
+    time = as.Date(row.names(dj_returns)),
     corr = c(copNorm$vCor,
             copT$vCor),
     lab = rep(c("Gaussian","Student's t"),
@@ -45,8 +45,8 @@ df <- data.frame(
 ggplot(data = df, aes(x=time, y=corr, col = lab)) + 
                                     geom_line() +
                                     labs(title = paste0("Correlation of Bivariate Elliptical Random Variable")) +
-                                    scale_x_date(date_labels = "%m-%Y") +
-                                    scale_x_date(date_breaks = "1 month", date_labels = "%M") +
+                                    scale_x_date(date_breaks = "1 year", date_minor_breaks = "1 month",
+                                    date_labels = "%m-%Y") + 
                                     labs(y = "Correlation") +
                                     theme_economist() +
                                     theme(legend.title=element_blank())
@@ -56,5 +56,5 @@ ggsave("./img/Correlation of Gaussian Copula and t Copula.pdf")
 # The correlation of the Gaussian copula is more stable than the correlation of the Student’s t copula.
 
 # Compare the two copula models using BIC. Which model is selected?
-sort(c(BIC_Gaussian = copNorm$BIC, BIC_t = copT$BIC))
+sort(c(BIC_Gaussian = copNorm$BIC, BIC_t = copT$BIC)) # lower BIC values are preferred
 # The Student’s t copula model is selected.
